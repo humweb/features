a1f6e79b906ad65cee16432b562e69ed23dc48df
Ryan Shofner <ryan@letsgolearn.com>
2015-06-24 00:21:08 -0700

    fix and cleanup DateTimeRangeStrategy

--- src/Strategy/DateTimeRangeStrategy.php
+++ src/Strategy/DateTimeRangeStrategy.php
@@ -7,71 +7,56 @@ namespace Humweb\Features\Strategy;
  */
 class DateTimeRangeStrategy extends AbstractStrategy
 {
-    /**
-     * Include start and end date time or not.
-     *
-     * @var bool
-     */
-    protected $inclusive;
-
-    /**
-     * Maximum date time.
-     *
-     * @var string
-     */
-    protected $maxRange;
-
-    /**
-     * Minimum date time.
-     *
-     * @var string
-     */
-    protected $minRange;
-
-
-    /**
-     * Constructor.
-     *
-     * @param string $minRange  Minimum date time.
-     * @param string $maxRange  Maximum date time.
-     * @param bool   $inclusive Include minimum and maximum dates in range.
-     */
-    public function __construct($minRange, $maxRange, $inclusive = false)
-    {
-        $this->inclusive = $inclusive;
-        $this->maxRange = $maxRange;
-        $this->minRange = $minRange;
-    }
-
 
     /**
      * {@inheritdoc}
      */
-    public function handle(array $args = [])
+    public function handle($args = [])
     {
-        $strategies = array();
-        $comparators = array('minRange' => '>', 'maxRange' => '<');
-
-        foreach ($comparators as $var => $comparator) {
-            if ($this->inclusive) {
-                $comparator .= '=';
-            }
-            $strategies[$var] = $this->createDateTimeStrategy($this->$var, $comparator);
-        }
-
         $result = true;
-        foreach ($strategies as $var => $strategy) {
-            if ( ! call_user_func($strategy, $feature)) {
-                $result = false;
+
+        $strict = $this->argGet($args, 'strict', true);
+
+        $dateRange = [
+            [
+                'date'     => $this->argGet($args, 'start'),
+                'operator' => '<'
+            ],
+            [
+                'date'     => $this->argGet($args, 'end'),
+                'operator' => '>'
+            ]
+        ];
+
+        foreach ($dateRange as $range) {
+
+            if ( ! $strict) {
+                $range['operator'] .= '=';
+            }
+
+            if ($this->runDateTimeStrategy($range['date'], $range['operator']) === false)
+            {
+                return false;
             }
         }
 
-        return $result;
+        return true;
     }
 
 
-    public function createDateTimeStrategy($datetime, $comparator)
+    public function runDateTimeStrategy($datetime, $operator)
     {
-        return new DateTimeStrategy($datetime, $comparator);
+        $strategy = new DateTimeStrategy();
+
+        $args = [
+            'datetime' => $datetime,
+            'operator' => $operator
+        ];
+
+        if ( ! call_user_func_array([$strategy, 'handle'], [$args])) {
+            return false;
+        }
+
+        return true;
     }
 }
