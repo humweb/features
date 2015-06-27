75a8fee7846f54d8ff0f447d3b7058b496d0b7e6
Ryan Shofner <ryan@letsgolearn.com>
2015-06-27 00:57:30 -0700

    Adds DaysOfWeek Strategy
    `DaysOfWeekStrategy` allows match days of the week (mon, tue, wed)

---
+++ src/Strategy/DaysOfWeekStrategy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Humweb\Features\Strategy;
+
+use DateTime;
+
+/**
+ * Days of week
+ * days-of-week
+ * days_of_week
+ */
+class DaysOfWeekStrategy extends AbstractStrategy
+{
+
+    protected $days = [
+        1 => ['mon', 'monday'],
+        2 => ['tue', 'tuesday'],
+        3 => ['wed', 'wednesday'],
+        4 => ['thu', 'thursday'],
+        5 => ['fri', 'friday'],
+        6 => ['sat', 'saturday'],
+        7 => ['sun', 'sunday']
+    ];
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle($args = [])
+    {
+
+        $days = (array)$this->argGet($args, 'days', []);
+
+        $dayNames = $this->getDayNames();
+
+        foreach ($days as $day) {
+            if ($this->daysMatch($dayNames, $day)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Get day name
+     *
+     * @return array
+     */
+    protected function getDayNames()
+    {
+        return $this->days[(new DateTime())->format('N')];
+    }
+
+
+    /**
+     * @param array  $todayNames
+     * @param string $matchDay
+     *
+     * @return bool
+     */
+    protected function daysMatch($todayNames, $matchDay)
+    {
+        return in_array(strtolower($matchDay), $todayNames);
+    }
+}
---
+++ tests/Strategy/DaysOfWeekStrategyTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Humweb\Features\Tests\Strategy;
+
+use DateTime;
+use Humweb\Features\StrategyCollection;
+
+/**
+ * Test DateTimeStrategy class
+ *
+ * @package Humweb\Features
+ */
+class DaysOfWeekStrategyTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $collection;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->collection = new StrategyCollection();
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        unset($this->collection);
+    }
+
+
+    /**
+     * @test
+     */
+    public function it_can_detect_a_single_day()
+    {
+        $this->collection->addStrategy('DayTest', 'days-of-week', [
+            'days' => [(new DateTime())->format('D')]
+        ]);
+        $this->assertEquals(true, $this->collection->check());
+
+        $this->collection->flush();
+    }
+
+
+    /**
+     * @test
+     */
+    public function it_can_detect_multiple_days()
+    {
+
+        $this->collection->addStrategy('DayTest', 'DaysOfWeek', [
+            'days' => [$this->getDayOtherThanToday(), (new DateTime())->format('D')]
+        ]);
+
+        $this->assertEquals(true, $this->collection->check());
+
+        $this->collection->flush();
+    }
+
+
+    /**
+     * @test
+     */
+    public function it_fails_when_no_days_match()
+    {
+
+        $this->collection->addStrategy('DayTest', 'DaysOfWeek', [
+            'days' => [$this->getDayOtherThanToday(), $this->getDayOtherThanToday()]
+        ]);
+        $this->assertEquals(false, $this->collection->check());
+
+        $this->collection->flush();
+    }
+
+
+    protected function getDayOtherThanToday()
+    {
+        $days  = ['Mon', 'Tue', 'Wed', 'Fri', 'Sat', 'Sun'];
+        $today = (new DateTime())->format('D');
+
+        foreach ($days as $day) {
+            if ($day !== $today) {
+                return $day;
+            }
+        }
+    }
+}
